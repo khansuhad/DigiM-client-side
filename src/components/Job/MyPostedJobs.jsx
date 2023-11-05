260d12390299b477117cc0b7b09758e75b9b14f1
khan suhad <suhadahmodkhan@gmail.com>
2023-11-06 01:44:22 +0600

    my posted job delete operation successfull

--- src/components/Job/MyPostedJobs.jsx
+++ src/components/Job/MyPostedJobs.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext,  useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 import { ToastContainer, toast } from 'react-toastify';
