de21c7f0678ebdcf473ac3bb9b832d0895a091e4
khan suhad <suhadahmodkhan@gmail.com>
2023-11-07 23:59:17 +0600

    readme file added with 5 feature of my project

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# React + Vite
+This project name is "DigiM"
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+here is my live link : [https://digim-19266.web.app](https://digim-19266.web.app)
+here is my vercel link : [https://assignment-11-server-side-rust.vercel.app](https://assignment-11-server-side-rust.vercel.app)
 
-Currently, two official plugins are available:
+Five feature of my project :
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- 1. this is a Freelancing Marketplace related project , here you see some of jobs , it will created by user , who searching for freelancer .
+- 2. We added firebase authentication system , here if you are client then your job will loaded by google or email,password and you can access private route.
+- 3. The private route is Add jobs , my posted jobs , my bids and bid request , if you are a client searching for freelancer you can add job from add job page , it will store in my posted jobs page , here you can update your job or permanently delete the job.
+- 4. Also freelancer can bid the job , jobs show will home page then if click bid it will redirect job details then a there freelancer can bid that job , if freelancer bid the job he can see this job in my bids , it status will pending if client accept bid request then it will in progress then you can complete your job and you can set completed status.
+- 5. There is another interesting page is success stories , There the user can post her experience or success story .It was really amazing thing .
+
+Thank you.
--- src/components/Home/Community.jsx
+++ src/components/Home/Community.jsx
@@ -24,7 +24,25 @@ const Community = () => {
             <hr  className="w-[90%]  mx-auto border-4 rounded"/>
 
             <div>
-                <h1 className="my-10 w-[90%] mx-auto text-xl font-semibold">Adding a skill assessment and training section can make your job posting website a one-stop destination for job seekers looking to enhance their skills and qualifications, making them more appealing to potential employers. It can also help you monetize your platform through course sales or partnerships with online learning providers</h1>
+                <h1 className="my-10 w-[90%] mx-auto text-xl font-semibold">DigiM - Your Freelance Marketplace
+
+Welcome to DigiM, the ultimate destination for connecting talented freelancers and skilled professionals with businesses and individuals in need of their expertise. Whether you are looking to get work done or looking to offer your services, our platform provides a seamless and efficient way to make it happen.
+
+For Clients:
+Are you in search of top-tier professionals to bring your projects to life? Look no further! At DigiM, you will find a vast pool of freelancers spanning various fields, including web development, graphic design, content writing, digital marketing, and more. Simply post your project, receive competitive bids, and choose the freelancer who best matches your needs and budget. You can collaborate, track progress, and pay securely, all within the platform.
+
+For Freelancers:
+Are you a freelancer with a unique skill set? DigiM offers you the opportunity to showcase your talents to a global audience. Create a captivating profile, list your services, and start bidding on projects that align with your expertise. Build your portfolio, earn reviews, and establish long-lasting relationships with clients in need of your skills. Plus, our platform handles secure payments, ensuring you get paid for your hard work.
+
+Why Choose DigiM?
+
+Diverse Talent Pool: Our platform hosts a diverse range of freelancers and professionals, ensuring that you will find the right match for your project.
+Secure Transactions: We prioritize safety and security. All transactions are handled with care and protection.
+Effortless Communication: Our messaging system makes it easy to communicate with freelancers and clients, keeping everyone on the same page.
+Transparent Reviews: Browse through reviews and ratings to make informed decisions when choosing who to work with.
+Project Management Tools: Use our integrated tools to manage your projects efficiently, ensuring they stay on track.
+Flexible Pricing: Find services and freelancers that fit your budget, with options ranging from fixed-price to hourly contracts.
+Join the community of talented professionals and clients today at DigiM and experience a new level of efficiency and collaboration in the freelance world. Get started now and unlock the endless possibilities of remote work and talent discovery!</h1>
             </div>
         </div>
     );
